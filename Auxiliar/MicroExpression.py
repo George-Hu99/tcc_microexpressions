143ff01f66fb0a083b139724db324027e38f8a5b
Henrique Haji <henrique.haji@gmail.com>
2018-11-03 12:06:13 -0300

    New :D

--- Auxiliar/MicroExpression.py
+++ Auxiliar/MicroExpression.py
@@ -1,30 +1,60 @@
-import os
-import operator
-from functools import reduce
-import numpy as np
-from PIL import Image
-import cv2
-import glob
-import matplotlib.pyplot as plt
+    import os
+    import operator
+    from functools import reduce
+    import numpy as np
+    from PIL import Image
+    import cv2
+    import glob
+    import matplotlib.pyplot as plt
+    import pandas as pd 
 
-from skimage.feature import hog
-from skimage import data, exposure
+    from skimage.feature import hog
+    from skimage import data, exposure
 
-from sklearn.decomposition import PCA
+    from sklearn.decomposition import PCA
 
 class MicroExpression(object):
-    def __init__(self, path):
+    CASME_ans = {
+        'happiness' : 0, 
+        'disgust' : 1, 
+        'repression' : 2, 
+        'fear' : 3, 
+        'sadness' : 4, 
+        'surprise' : 5, 
+        'others' : 6
+    }
+
+    CASME_to_SMIC = {
+        'happiness' : 0, 
+        'disgust' : 1, 
+        'repression' : 1, 
+        'fear' : 1, 
+        'sadness' : 1, 
+        'surprise' : 2, 
+        'others' : 3
+    }
+
+    SMIC_ans = {
+        'positive' : 0,
+        'negative' : 1,
+        'surprise' : 2,
+        'default' : 3
+    }
+
+    def __init__(self, path, w=120, h=150):
         self.path = path
         self.image_list = []
         self.filename = []
         for i, filename in enumerate(os.listdir(path)):
-            im = np.array(Image.open('{path}/{filename}'.format(path=path, filename=filename)).convert('L'))
+            im = Image.open('{path}/{filename}'.format(path=path, filename=filename))
+            im = im.resize((w,h), Image.ANTIALIAS)
+            im = np.array(im.convert('L'))
             self.image_list.append(im)
             self.filename.append(filename)
 
         self.qtd_images = len(self.image_list)
 
-        self.dataset = "SMIC_all_cropped" if self.path.find("SMIC_all_cropped") != -1 else "Cropped_norm"
+        self.dataset = "SMIC_all_cropped" if self.path.find("SMIC_all_cropped") != -1 else "Cropped"
 
         if self.dataset == "SMIC_all_cropped":
             if self.path.find("HS") != -1:
@@ -39,30 +69,43 @@ class MicroExpression(object):
 
             if self.path.find("positive") != -1:
                 self.type = "positive"
-                self.ans = 3
+                self.ans_bool = 1
             elif self.path.find("negative") != -1:
-                self.type = "negative"
-                self.ans = 2
+                self.type = "negative"                
+                self.ans_bool = 1
             elif self.path.find("surprise") != -1:
                 self.type = "surprise"
-                self.ans = 1
+                self.ans_bool = 1
             else:
                 self.type = "default"
-                self.ans = 0
+                self.ans_bool = 0
 
+            self.ans_smic = SMIC_ans[self.type]
         else:
             self.camera = "no_cam"
             self.type = "default"
-            self.ans = 0
+            self.ans_bool = 0
+    else:
+        self.camera = "HS"
+        self.micro = 'micro'
 
-        #hog = cv.HOGDescriptor()
-        #self.hog_array = list(map(lambda x: hog.compute(x), self.image_list))
+        sub = self.path[self.path.find('sub')+3 : self.path.find('sub')+5]
+        ans_df = pd.read_csv('CASME2_Ans.csv', ',')
+        name = self.path[self.path.find('EP') : len(self.path)]
+        data = ans_df[(ans_df.Subject == int(sub)) & (ans_df.Filename == name)]
+        self.type = data.EstimatedEmotion
+    
+        self.ans_smic = CASME_to_SMIC[self.type]
+        self.ans_casme = CASME_ans[self.type]
+        if self.type = 'others':
+            self.ans_bool = 0
+        else:
+            self.ans_bool = 1
 
     def apply_hog(self, orientations=9, pixels_per_cell=(10, 10),
 	cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1",
 	visualize=False, _PCA=False, pca_lvl=.95):      
-        #self.hog_array = list(map(lambda x: hog.compute(x), self.image_list))
-        
+
         self.hog_array = list(map(lambda x: 
             hog(x, 
                 orientations=orientations, 
--- __init__.py
+++ __init__.py
@@ -6,26 +6,22 @@ import pickle
 import cv2
 from sklearn.model_selection import train_test_split
 from itertools import *
-from sklearn.model_selection import cross_val_score
-from sklearn import svm
-from keras.models import Sequential
-from sklearn.ensemble import RandomForestClassifier
 
 # Muda o tamanho de várias imagens abaixo de um diretório
 def rescale(dir):
     for filename in os.listdir(dir):
         if os.path.isdir(os.path.join(dir, filename)):
             rescale(os.path.join(dir, filename))
-        elif filename.endswith(".jpg") and os.listdir(dir)[0].endswith(".jpg"):
-            os.system("mogrify -resize 131x161 -quality 100 -format bmp  {0}/*.jpg".format(dir))
-            os.system("find {0} -name '*.jpg' -delete".format(dir))
+        elif filename.endswith(".bmp") and os.listdir(dir)[0].endswith(".bmp"):
+            os.system("mogrify -resize 120x150 -quality 100 -format bmp  {0}/*.bmp".format(dir))
+            #os.system("find {0} -name '*.jpg' -delete".format(dir))
 
 # Gera uma string para cada pasta de microexpressão
 def getImages(diretorio, path_imgs):
     for filename in os.listdir(diretorio):
         if os.path.isdir(os.path.join(diretorio, filename)):
             getImages(os.path.join(diretorio, filename), path_imgs)
-        elif filename.endswith('.bmp'):
+        elif filename.endswith('.bmp') or filename.endswith('.jpg'):
             path_imgs.append(diretorio)
             break
     return path_imgs
@@ -47,72 +43,64 @@ def make_equal(arrays, tam_max):
         if len(array) < tam_max:
             array = np.append(array, (tam_max-len(array)) * [0])
     return arrays
-            
+
             
 
 if __name__ == "__main__":
-    dir = '/home/henriquehaji/PycharmProjects/TCC/PreProcess/Datasets/SMIC_all_cropped/'
-    path_imgs = getImages(dir, list())
+    dir_smic = '/home/henriquehaji/PycharmProjects/TCC/PreProcess/Datasets/SMIC_all_cropped/HS'
+        
+    
+    path_imgs = getImages(dir_smic, list())
+    path_imgs = getImages(dir_norm, path_imgs)
+
     #hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,
     #derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,
     #nlevels, signedGradients)
-    maior = 0
     X = []
     with open('hog_arrays.txt', 'a') as file:
         for i, path in enumerate(path_imgs):
-            #print(i)
             micro = MicroExpression(path)
             micro.apply_hog(_PCA=False)
             micro.summarize_hog(1)
-
-            #file.write(','.join(map(str,micro.s_hog)) + '\n')
-
             X.append(micro.s_hog)
-
-
-            if len(X[i]) > maior:
-                maior = len(X[i])
-            #for j, array in enumerate(micro.hog_array):
-                #file.write(','.join(map(str,micro.hog_array[j])) + '\n')
-            #object_to_file((micro.hog_array,micro.type), 'hog_arrays/{}'.format(i))
-
-    for i, array in enumerate(X):
-        if len(array) < maior:
-            X[i] = np.append(array, (maior-len(array)) * [0])
-
     y = []
     with open('answers.txt', 'a') as file:
         for i, path in enumerate(path_imgs):
             micro = MicroExpression(path)
             y.append(micro.ans)
-            #file.write(''.join(micro.type) + '\n')
     
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    #SVM
-    clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-    acertos = sum(clf.predict(X_test) == y_test)
-    scores = cross_val_score(clf, X_test, y_test, cv=50)
+    #SVM Linear
+    clf_svm_l = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+    acertos = sum(clf_svm_l.predict(X_test) == y_test)
+    scores = cross_val_score(clf_svm_l, X_test, y_test, cv=50)
+
+    #SVM RBF
+    clf_svm_rbf = svm.SVC(kernel='rbf', C=1).fit(X_train, y_train)
+    acertos = sum(clf_svm_rbf.predict(X_test) == y_test)
+    scores = cross_val_score(clf_svm_rbf, X_test, y_test, cv=50)
 
     #Random Forest
-    clf = RandomForestClassifier(n_jobs=2, random_state=0).fit(X_train, y_train)
-    acertos = sum(clf.predict(X_test) == y_test)
-    scores = cross_val_score(clf, X_test, y_test, cv=50)
+    clf_rf = RandomForestClassifier(n_jobs=3, random_state=0).fit(X_train, y_train)
+    acertos = sum(clf_rf.predict(X_test) == y_test)
+    scores = cross_val_score(clf_rf, X_test, y_test, cv=50)
     
     #Deep
     from keras.utils import to_categorical
+    from keras.layers import Dense
     y_binary = to_categorical(y_train)
 
     model = Sequential()
 
-    model.add(Dense(units=1, activation='relu', input_dim=11520))
-    model.add(Dense(units=4, activation='softmax'))
+    model.add(Dense(units=1, activation='relu', input_shape=(1, 5544)))
+    model.add(Dense(units=1, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
-                optimizer='sgd',
+                optimizer='rmsprop',
                 metrics=['accuracy'])
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
-    model.fit(np.array(X_train), np.array(y_binary), epochs=5, batch_size=32)
+    #model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+    #            optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+    model.fit(np.array(X_train), np.array(y_train), epochs=5, batch_size=32)
     
 
     
